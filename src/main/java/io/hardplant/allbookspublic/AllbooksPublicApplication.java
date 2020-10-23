8d775a81c892221b6d952e5d4da68375e96311fc
seongwon <abc7988se@gmail.com>
2020-10-23 15:22:35 +0900

    Initial Structure

---
+++ src/main/java/io/hardplant/allbookspublic/AllbooksPublicApplication.java
@@ -0,0 +1,13 @@
+package io.hardplant.allbookspublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbooksPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbooksPublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hardplant/allbookspublic/AllbooksPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hardplant.allbookspublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllbooksPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
